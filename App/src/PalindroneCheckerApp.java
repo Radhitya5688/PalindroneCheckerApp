b91d470386088772c312dac686edd8f6c070afba
Radhitya5688 <radhityavarma2006@gmail.com>
2026-02-20 10:11:16 +0530

    Radhitya Varma Added UC1-WelcomePage

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,9 @@
+public class PalindroneCheckerApp{
+            public static void main(String[] args) {
+                System.out.println("welcome to Palindrone Checker App");
+                System.out.println("Application name: Palindrone Checker");
+                System.out.println("version         :chitti2.0(red chip)");
+
+                System.out.println("system Initialized sucessfully");
+            }
+        }
